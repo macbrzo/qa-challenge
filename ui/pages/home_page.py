c2fd021618308a3f8d2d4a23f83be8ffa4f0d44e
Maciej Brzostowski <maciejbrzost@gmail.com>
2025-02-06 21:42:10 +0100

    Adjust typing to 3.12

--- ui/pages/home_page.py
+++ ui/pages/home_page.py
@@ -1,6 +1,8 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+
 from ui.pages.twitch_base_page import TwitchTVPage
 
 
 class HomePage(TwitchTVPage):
-    def __init__(self, driver):
+    def __init__(self, driver: WebDriver):
         super().__init__(driver)
