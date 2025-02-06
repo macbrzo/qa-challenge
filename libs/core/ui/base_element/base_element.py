5182443c256b70cd347a9acc73b3fcd1b380c1b2
Maciej Brzostowski <maciejbrzost@gmail.com>
2025-02-06 21:12:38 +0100

    Update typing

--- libs/core/ui/base_element/base_element.py
+++ libs/core/ui/base_element/base_element.py
@@ -2,8 +2,8 @@ import re
 import time
 
 from selenium.common.exceptions import (
-	ElementClickInterceptedException,
-	TimeoutException,
+    ElementClickInterceptedException,
+    TimeoutException,
 )
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
@@ -15,88 +15,88 @@ import config
 
 
 class BaseElement:
-	def __init__(
-		self,
-		driver,
-		locator: str | None = None,
-		parent: 'BaseElement' | None = None,
-		web_element: WebElement | None = None,
-		timeout: int = config.global_timeout,
-		logger=None,
-	):
-		if not (web_element or locator):
-			raise ValueError('Either locator or web_element must be provided')
+    def __init__(
+        self,
+        driver,
+        locator: str | None = None,
+        parent: "BaseElement" | None = None,
+        web_element: WebElement | None = None,
+        timeout: int = config.global_timeout,
+        logger=None,
+    ):
+        if not (web_element or locator):
+            raise ValueError("Either locator or web_element must be provided")
 
-		self.driver = driver
-		self.locator = None if not locator else self.best_locator(locator=locator)
-		self.web_element = web_element
-		self.parent = parent
-		self.timeout = timeout
-		self.logger = logger
+        self.driver = driver
+        self.locator = None if not locator else self.best_locator(locator=locator)
+        self.web_element = web_element
+        self.parent = parent
+        self.timeout = timeout
+        self.logger = logger
 
-	def _get_location_context(self) -> WebDriver | WebElement:
-		if self.parent:
-			return self.parent.element
-		return self.driver
+    def _get_location_context(self) -> WebDriver | WebElement:
+        if self.parent:
+            return self.parent.element
+        return self.driver
 
-	@staticmethod
-	def best_locator(*, locator: str) -> tuple[str, str]:
-		pattern = r'(?P<by>css|xpath)=(?P<selector>.+)'
-		matches = re.match(pattern, locator)
-		if not matches:
-			raise AttributeError(f'Wrong locator provided {locator=}')
-		by = {
-			'css': By.CSS_SELECTOR,
-			'xpath': By.XPATH,
-		}[matches.group('by')]
-		return by, matches.group('selector')
+    @staticmethod
+    def best_locator(*, locator: str) -> tuple[str, str]:
+        pattern = r"(?P<by>css|xpath)=(?P<selector>.+)"
+        matches = re.match(pattern, locator)
+        if not matches:
+            raise AttributeError(f"Wrong locator provided {locator=}")
+        by = {
+            "css": By.CSS_SELECTOR,
+            "xpath": By.XPATH,
+        }[matches.group("by")]
+        return by, matches.group("selector")
 
-	@property
-	def element(self) -> WebElement:
-		if self.web_element:
-			return self.web_element
+    @property
+    def element(self) -> WebElement:
+        if self.web_element:
+            return self.web_element
 
-		location_context = self._get_location_context()
-		try:
-			return WebDriverWait(
-				driver=location_context,
-				timeout=self.timeout,
-				poll_frequency=0.2,
-			).until(EC.presence_of_element_located(self.locator))
-		except TimeoutException:
-			raise TimeoutException(f'Element not found: {self.locator}')
+        location_context = self._get_location_context()
+        try:
+            return WebDriverWait(
+                driver=location_context,
+                timeout=self.timeout,
+                poll_frequency=0.2,
+            ).until(EC.presence_of_element_located(self.locator))
+        except TimeoutException:
+            raise TimeoutException(f"Element not found: {self.locator}")
 
-	def is_present(self) -> bool:
-		if self.web_element:
-			return self.web_element.is_displayed()
+    def is_present(self) -> bool:
+        if self.web_element:
+            return self.web_element.is_displayed()
 
-		location_context = self._get_location_context()
-		element = location_context.find_elements(*self.locator)
-		return bool(element)
+        location_context = self._get_location_context()
+        element = location_context.find_elements(*self.locator)
+        return bool(element)
 
-	def click(self, attempts: int = 5) -> None:
-		element = self.element
-		if not element.is_enabled():
-			raise TimeoutException(f'Element not enabled: {self.locator}')
+    def click(self, attempts: int = 5) -> None:
+        element = self.element
+        if not element.is_enabled():
+            raise TimeoutException(f"Element not enabled: {self.locator}")
 
-		exception = None
-		for _ in range(attempts):
-			try:
-				element.click()
-				break
-			except ElementClickInterceptedException as e:
-				if not exception:
-					exception = e
-				time.sleep(1)
-		else:
-			raise exception from None
+        exception = None
+        for _ in range(attempts):
+            try:
+                element.click()
+                break
+            except ElementClickInterceptedException as e:
+                if not exception:
+                    exception = e
+                time.sleep(1)
+        else:
+            raise exception from None
 
-	def find_all_elements_in_viewport(self) -> list['BaseElement']:
-		elements = WebDriverWait(self.driver, self.timeout).until(
-			EC.visibility_of_all_elements_located(self.locator)
-		)
+    def find_all_elements_in_viewport(self) -> list["BaseElement"]:
+        elements = WebDriverWait(self.driver, self.timeout).until(
+            EC.visibility_of_all_elements_located(self.locator)
+        )
 
-		js_script = """
+        js_script = """
         function isInViewport(element) {
             const rect = element.getBoundingClientRect();
             return (
@@ -108,17 +108,17 @@ class BaseElement:
         }
         return isInViewport(arguments[0]);
         """
-		visible_elements = []
-		for element in elements:
-			if self.driver.execute_script(js_script, element):
-				visible_elements.append(BaseElement(driver=self.driver, web_element=element))
-		return visible_elements
+        visible_elements = []
+        for element in elements:
+            if self.driver.execute_script(js_script, element):
+                visible_elements.append(BaseElement(driver=self.driver, web_element=element))
+        return visible_elements
 
-	def send_keys(self, value: str) -> None:
-		element = self.element
-		element.clear()
-		element.send_keys(value)
+    def send_keys(self, value: str) -> None:
+        element = self.element
+        element.clear()
+        element.send_keys(value)
 
-	def wait_for_element_to_be_visible(self, timeout: int = 10) -> None:
-		location_context = self._get_location_context()
-		WebDriverWait(location_context, timeout=timeout).until(EC.visibility_of(self.element))
+    def wait_for_element_to_be_visible(self, timeout: int = 10) -> None:
+        location_context = self._get_location_context()
+        WebDriverWait(location_context, timeout=timeout).until(EC.visibility_of(self.element))
